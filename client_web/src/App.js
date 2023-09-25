899e5b7dd69a9e13436d2746fe5b6bf01053bdcb
sam <samuel.florentin@epitech.eu>
2023-09-25 11:34:22 +0200

    [ADD] first file for login page

--- client_web/src/App.js
+++ client_web/src/App.js
@@ -3,7 +3,17 @@ import "./styles/App.css";
 import loginScreen from "./screens/loginScreen";
 function App() {
   return (
-      <p>aaaaaaa</p>
+    <div className="box">
+      <div className="CircleBlu" />
+      <div className="CircleBlu2" />
+      <div className="CircleHollow" />
+      <div className="CircleHollow2" />
+      <div className="CirclePink" />
+      <div className="CirclePink2" />
+      <div className="App">
+        <d className="Text"> taaddddtddddfff </d>
+      </div>
+    </div>
   );
 }
 
