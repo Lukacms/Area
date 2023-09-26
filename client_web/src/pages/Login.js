fb40dbfedb4a845983bb3bf24ce7230a57102162
sam <samuel.florentin@epitech.eu>
2023-09-26 11:05:10 +0200

    [FIX] change absolute to fixed and begin to to the login page

--- client_web/src/pages/Login.js
+++ client_web/src/pages/Login.js
@@ -1,23 +1,34 @@
 import '../styles/login.css';
 import useLogin from '../hooks/useLogin';
-import "../styles/App.css";
+import '../styles/App.css';
+import {React, useState} from 'react';
+import { Divider } from 'primereact/divider';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 
 function Login() {
   const {} = useLogin();
+    const [Email, setEmail] = useState("");
 
   return (
-    <div>
     <div className="box">
-      <div className="CircleBlu" />
-      <div className="CircleBlu2" />
-      <div className="CircleHollow" />
-      <div className="CircleHollow2" />
-      <div className="CirclePink" />
-      <div className="CirclePink2" />
-      <div className="App">
-        <d className="Text"> taaddddtddddfff </d>
+      <div className="overflow: hidden">
+        <div className="CircleBlu" />
+        <div className="CircleBlu2" />
+        <div className="CircleHollow" />
+        <div className="CircleHollow2" />
+        <div className="CirclePink" />
+        <div className="CirclePink2" />
+
+        <p className='Text'> {Email} </p>
+        <div className="App">
+          <label className="EmailBox">
+            Email:
+            <InputText className='TextBox' value={Email} onChange={(e) => setEmail(e.target.value)}/>
+          </label>
+          <label className="PassBox">Password:</label>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
