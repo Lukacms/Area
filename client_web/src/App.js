b0f454c3150457f8e6a2b1e270a438984fe256d8
sam <samuel.florentin@epitech.eu>
2023-10-02 10:49:40 +0200

    [ADD] home page

--- client_web/src/App.js
+++ client_web/src/App.js
@@ -1,15 +1,17 @@
 import "./styles/App.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from "./pages/Login";
+import Home from "./pages/home";
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.css';
-import './styles/theme.css';
+import 'primereact/resources/themes/mdc-dark-deeppurple/theme.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- client_web/src/pages/Login.js
+++ client_web/src/pages/Login.js
@@ -1,17 +1,23 @@
 import '../styles/login.css';
 import useLogin from '../hooks/useLogin';
-import '../styles/App.css';
 import { React, useState } from 'react';
 import { Image } from 'primereact/image';
 import logo from '../FastR 2.png';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const {} = useLogin();
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
+  const navigate = useNavigate();
 
+  function onPress() {
+    navigate('/home');
+  }
+
+  function onPressForget() {}
   return (
     <div className='box'>
       <div className='CircleBlu' />
@@ -24,8 +30,8 @@ function Login() {
       <p className='Text'> {email} </p>
       <p className='Text'> {pass} </p>
       <div className='App'>
-        <Image src={logo} alt='Image' className='Img' width='272px' height='186px' />
         <div className='divLogin'>
+          <Image src={logo} alt='Image' width='273px' height='186px' className='img' />
           <label className='EmailBox'>
             Email:
             <InputText
@@ -44,11 +50,24 @@ function Login() {
               type='password'
               placeholder='Enter your password'
             />
+            <p className='fPassword' onClick={onPressForget}>
+              {' '}
+              Forget password ?
+            </p>
           </label>
-          <Button label='Login' className='buttonDiv' />
+          <Button label='Login' className='buttonDiv' left='' onClick={onPress} />
+        </div>
+        <div className='divDiv'>
+          <hr className='hrMidle' />
+        </div>
+        <div className='divSignin'>
+          <p className='title'>
+            <b>Dont have an account ?</b>
+          </p>
+          <p className='subTitle'> sign up now !</p>
+          <Button label='Sign up' className='buttonDiv2' />
         </div>
       </div>
-      <hr className='hrMidle' />
     </div>
   );
 }
