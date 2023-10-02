2c472dfaaa0e9f00add39138aa6850bee817954d
sam <samuel.florentin@epitech.eu>
2023-10-02 12:16:27 +0200

    [ADD] button setting

--- client_web/src/hooks/useHome.js
+++ client_web/src/hooks/useHome.js
@@ -1,6 +1,163 @@
+import { useEffect, useState } from 'react';
 
-function useHome() {
-    return {};
-}
+const useHome = () => {
+  const [addAct, setaddAct] = useState(false);
+  const [area, setArea] = useState([{ action: '', reaction: '', name: '' }]);
+  const [tmpAct, setTmp] = useState('');
+  const [selectedArea, setSelectedArea] = useState({action: '', reaction: ''});
+
+  const clickAct = (eventName) => {
+    if (addAct) {
+      /*console.log("Please select a reaction");*/
+      return;
+    }
+    setTmp(eventName);
+    setaddAct(true);
+  };
+
+  const clickReact = (eventName) => {
+    if (!addAct) {
+      console.log("Please select an action first");
+      return;
+    }
+    area.push({ action: tmpAct, reaction: eventName, name: "act" });
+    setaddAct(false);
+  };
+
+  useEffect(() => {
+    const tmpActMod = () => {
+      console.log(tmpAct);
+    };
+    const addActMod = () => {
+      console.log(addAct);
+    };
+    tmpActMod();
+    addActMod();
+  }, [clickAct]);
+
+  useEffect(() => {
+    const areaMod = () => {
+        console.log("area: ", area);
+      /*for (let i = 0; i < area.length; i++)
+        console.log('ff:', area[i].action, ' ', 'gg: ', area[i].reaction);*/
+    };
+    areaMod();
+    addToAreas(area[area.length - 1]);
+  }, [clickReact]);
+
+  const items = [
+    {
+      label: 'discord',
+      items: [
+        {
+          label: 'action disc 1',
+          command: (event) => {
+            clickAct(items[0].items[0].label);
+          },
+        },
+        {
+          label: 'action disc 2',
+          command: (event) => {
+            clickAct(items[0].items[1].label);
+          },
+        },
+      ],
+    },
+    {
+      label: 'microsoft',
+      items: [
+        {
+          label: 'action mail1',
+          command: (event) => {
+            clickAct(items[1].items[0].label);
+          },
+        },
+        {
+          label: 'action mail2',
+          command: (event) => {
+            clickAct(items[1].items[1].label);
+          },
+        },
+      ],
+    },
+  ];
+
+  const items2 = [
+    {
+      label: 'discord',
+      items: [
+        {
+          label: 'reaction disc 1',
+          command: (event) => {
+            clickReact(items2[0].items[0].label);
+          },
+        },
+        {
+          label: 'reaction disc 2',
+          command: (event) => {
+            clickReact(items2[0].items[1].label);
+          },
+        },
+      ],
+    },
+    {
+      label: 'microsoft',
+      items: [
+        {
+          label: 'reaction mail1',
+          command: (event) => {
+            clickReact(items2[1].items[0].label);
+          },
+        },
+        {
+          label: 'reaction mail2',
+          command: (event) => {
+            clickReact(items2[1].items[1].label);
+          },
+        },
+      ],
+    },
+  ];
+
+  const areas = [
+      {
+          label: "fav",
+          items: [
+              {
+                  label: "",
+                  data: {action: "", reaction: ""}
+              }
+          ]
+      },
+      {
+          label: "not fav",
+          items: [
+              {
+                  label: "",
+                  data: {action: "", reaction: ""}
+              }
+          ]
+      }
+  ]
+
+    const addToAreas = ({act, react, name}) => {
+        if (areas[1].items[0].label === "") {
+            areas[1].items.pop();
+            areas[1].items.push({label: name, data: {action: act, reaction: react}});
+        } else {
+            areas[1].items.push({label: name, data: {action: act, reaction: react}});
+        }
+    }
+
+  const [items3, setItems] = useState(items);
+
+  const dispAct = () => {
+    setItems(items);
+  };
+  const dispReac = () => {
+    setItems(items2);
+  };
+  return { dispAct, dispReac, items3, areas, selectedArea };
+};
 
 export default useHome;
--- client_web/src/pages/home.js
+++ client_web/src/pages/home.js
@@ -1,148 +1,32 @@
 import '../styles/home.css';
 import useHome from '../hooks/useHome';
-import { useEffect, useState } from 'react';
 import { Button } from 'primereact/button';
 import { PanelMenu } from 'primereact/panelmenu';
 import { Divider } from 'primereact/divider';
+import { useNavigate } from 'react-router-dom';
 
-function Home() {
-  const {} = useHome();
-    const [addAct, setaddAct] = useState(false);
-    const [area, setArea] = useState([{action: "", reaction: ""}]);
-    const [tmpAct, setTmp] = useState("");
-
-    const clickAct = (eventName) => {
-        if (addAct) {
-            console.log("Please select a reaction");
-            return;
-        }
-        setTmp(eventName);
-        setaddAct(true);
-    }
-
-    const clickReact = (eventName) => {
-        if (!addAct) {
-            console.log("Please select an action first");
-            return;
-        }
-        area.push({action: tmpAct, reaction: eventName});
-        setaddAct(false);
-    }
-
-    useEffect(() => {
-        const tmpActMod = () => {
-            console.log(tmpAct);
-        }
-        const addActMod = () => {
-            console.log(addAct);
-        }
-        tmpActMod();
-        addActMod();
-    }, [clickAct])
-
-    useEffect(() => {
-        const areaMod = () => {
-            console.log(area);
-            console.log("area length: ", area.length);
-            for (let i = 0; i < area.length; i++)
-                console.log("ff:", area[i].action, " ", "gg: ", area[i].reaction);
-        }
-        areaMod();
-    }, [clickReact])
-
-
-
-  const items = [
-    {
-      label: 'discord',
-      items: [
-        {
-          label: 'action disc 1',
-          command: (event) => {clickAct(items[0].items[0].label);}
-        },
-        {
-          label: 'action disc 2',
-          command: (event) => {clickAct(items[0].items[1].label);}
-        },
-      ],
-    },
-    {
-      label: 'microsoft',
-      items: [
-        {
-          label: 'action mail1',
-          command: (event) => {clickAct(items[1].items[0].label);}
-        },
-        {
-          label: 'action mail2',
-          command: (event) => {clickAct(items[1].items[1].label);}
-        },
-      ],
-    },
-  ];
-
-
-function fffff()
-    {
-        console.log("gg");
-    }
-
-const items2 = [
-    {
-      label: 'discord',
-      items: [
-        {
-          label: 'reaction disc 1',
-          command: (event) => {clickReact(items2[0].items[0].label);}
-        },
-        {
-          label: 'reaction disc 2',
-          command: (event) => {clickReact(items2[0].items[1].label);}
-        },
-      ],
-    },
-    {
-      label: 'microsoft',
-      items: [
-        {
-          label: 'reaction mail1',
-          command: (event) => {clickReact(items2[1].items[0].label);}
-        },
-        {
-          label: 'reaction mail2',
-          command: (event) => {clickReact(items2[1].items[1].label);}
-        },
-      ],
-    },
-  ];
-
-    const [items3, setItems] = useState(items);
-
-    function dispAct() {
-        setItems(items);
-    }
-    function dispReac() {
-        setItems(items2);
-    }
-
-
-    function onDragEnd(e) {
-        console.log(e);
-    }
+const Home = () => {
+  const {dispAct, dispReac, items3, areas, selectedArea} = useHome();
+  const navigate = useNavigate();
 
   return (
     <div className='globalDiv'>
       <div className='leftDiv'>
+      <div className='topLeft'>
         <b className='fastrText'> FastR </b>
+        <Button label='setting' onClick={() => navigate('/settings')}/>
+      </div>
         <div className='selectButton'>
           <Button label='Actions' onClick={dispAct}/>
           <Button label='Reactions' onClick={dispReac}/>
         </div>
         <Divider />
-        <PanelMenu model={items3} className='pannelMenu' onDragEnd={e => {onDragEnd(e.target.dispatchEvent)}}/>
+        <PanelMenu model={items3} className='pannelMenu'/>
       </div>
       <div className='middleDiv'></div>
-      <div className='rightDiv'></div>
+      <div className='rightDiv'>
+      <PanelMenu model={areas} className='pannelMenu' />
+      </div>
     </div>
   );
 }
