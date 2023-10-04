45284a0fe57c72ebe9a24ad48dd90ff91a5df4ce
Luka Camus <luka.camus@epitech.eu>
2023-10-04 16:52:45 +0200

    [MODIFY] Login => completed Login page and connect to back
    get token and put it in secure storage

--- client_web/src/config/request.js
+++ client_web/src/config/request.js
@@ -1,20 +1,21 @@
 import axios from 'axios';
 import { API_URL } from './constants';
+import secureLocalStorage from 'react-secure-storage';
 
 // used to refresh token when getting 403 error
-// const refreshToken = () => {};
+const refreshToken = () => {};
 
-export const axiosInstance = (token) =>
-  axios.create({
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    baseURL: API_URL,
-  });
+export const axiosInstance = axios.create({
+  headers: {
+    Authorization: `Bearer ${secureLocalStorage.getItem('token')}`,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
+});
 
 // causing the pages to crash, idk why
 // intercept 403 code to refresh token
-/* axios.interceptors.response.use(
+axios.interceptors.response.use(
   (response) => {
     return response;
   },
@@ -23,15 +24,22 @@ export const axiosInstance = (token) =>
       refreshToken();
     }
   },
-); */
+);
 
-export const getUsers = (token) => axiosInstance(token, 'Users').get();
+export const getUsers = () => axiosInstance.get('Users');
 
-export const login = (userData) =>
+export const register = (userData) =>
   axios.post(API_URL + 'Users/register', userData, {
     headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true },
   });
 
-export const getServices = (token) => axiosInstance(token).get(API_URL + 'Services');
+export const login = (userData) =>
+  axios.post(API_URL + 'Users/login', userData, {
+    headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true },
+  });
 
-export const getUserServices = (token, id) => axiosInstance(token).get(API_URL + `UserServices/${id}`);
+export const getServices = () => axiosInstance.get(API_URL + 'Services');
+
+export const getUserServices = (id) => axiosInstance.get(API_URL + `UserServices/${id}`);
+
+export const getMyProfile = () => axiosInstance.get(API_URL + 'Users/me');
--- client_web/src/pages/Login.js
+++ client_web/src/pages/Login.js
@@ -1,53 +1,72 @@
-import React, { useState } from 'react';
-import useLogin from '../hooks/useLogin';
+import React from 'react';
 import { Image } from 'primereact/image';
-import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { Formik, Form, Field } from 'formik';
+import { useLogin } from '../hooks/';
+import { Background, FormikInputtext, FormikPassword } from '../components';
 import '../styles/login.css';
 
 function Login() {
-  const { login, navigate } = useLogin();
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+  const { loginUser, navigate, initialValues, validate, loading, error, setError } = useLogin();
 
   return (
-    <div className='box'>
-      <div className='CircleBlu' />
-      <div className='CircleBlu2' />
-      <div className='CircleHollow' />
-      <div className='CircleHollow2' />
-      <div className='CirclePink' />
-      <div className='CirclePink2' />
-
-      <p className='Text'> {email} </p>
-      <p className='Text'> {pass} </p>
+    <Background>
       <div className='App'>
         <div className='divLogin'>
-          <Image src={process.env.PUBLIC_URL + 'icon.png'} alt='Image' width='273px' height='186px' className='img' />
-          <label className='EmailBox'>
-            Email:
-            <InputText
-              className='TextBox'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder='Enter your email'
-            />
-          </label>
-          <label className='PassBox'>
-            Password:
-            <InputText
-              className='TextBox'
-              value={pass}
-              onChange={(e) => setPass(e.target.value)}
-              type='password'
-              placeholder='Enter your password'
-            />
-            <p className='fPassword' onClick={() => navigate('/')}>
-              {' '}
-              Forgot your password ?
-            </p>
-          </label>
-          <Button label='Login' className='buttonDiv' left='' onClick={() => login()} />
+          <Image
+            src={process.env.PUBLIC_URL + 'icon.png'}
+            alt='Image'
+            width='273px'
+            height='186px'
+            className='img'
+          />
+          <Formik initialValues={initialValues} validationSchema={validate} onSubmit={loginUser}>
+            {(props) => (
+              <Form>
+                <div className='textBox'>
+                  <Field
+                    name='email'
+                    type='email'
+                    as={FormikInputtext}
+                    label='Email'
+                    placeholder='Your email'
+                    error={props.errors?.email}
+                    touched={props.touched?.email}
+                  />
+                </div>
+                <div className='textBox'>
+                  <Field
+                    name='password'
+                    type='password'
+                    as={FormikPassword}
+                    label='Password'
+                    placeholder='Your password'
+                    feedback={false}
+                    error={props.errors?.password}
+                    touched={props.touched?.password}
+                  />
+                </div>
+                <Button
+                  label='I forgot my password'
+                  link
+                  size='small'
+                  severity='info'
+                  type='button'
+                  onClick={() => navigate('/login#forgot')}
+                />
+                <br />
+                <br />
+                <Button
+                  label='Log In'
+                  type='submit'
+                  icon={loading ? 'pi' : 'pi pi-check'}
+                  iconPos='right'
+                  className='buttonDiv'
+                />
+              </Form>
+            )}
+          </Formik>
         </div>
         <div className='divDiv'>
           <hr className='hrMidle' />
@@ -56,11 +75,21 @@ function Login() {
           <p className='title'>
             <b>Dont have an account ?</b>
           </p>
-          <p className='subTitle'> sign up now !</p>
-          <Button label='Sign up' className='buttonDiv2' onClick={() => navigate('/register')} />
+          <p className='subTitle'>Sign up now !</p>
+          <Button
+            label='Sign up'
+            size='large'
+            className='buttonDiv2'
+            icon='pi pi-chevron-right'
+            iconPos='right'
+            onClick={() => navigate('/register')}
+          />
         </div>
       </div>
-    </div>
+      <Dialog header='Failure' visible={error ? true : false} onHide={() => setError(null)}>
+        <span>{error}</span>
+      </Dialog>
+    </Background>
   );
 }
 
