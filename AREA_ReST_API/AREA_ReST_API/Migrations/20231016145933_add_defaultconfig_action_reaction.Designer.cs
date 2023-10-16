4ce50ace31cf7d844796863a1042fc5b6dfcb44b
Kane <louis.bassagal@epitech.eu>
2023-10-16 17:00:24 +0200

    [ADD] Add DefaultConfiguration model in Action and Reaction table

--- AREA_ReST_API/AREA_ReST_API/Migrations/20231016145933_add_defaultconfig_action_reaction.Designer.cs
+++ AREA_ReST_API/AREA_ReST_API/Migrations/20231016145933_add_defaultconfig_action_reaction.Designer.cs
@@ -2,6 +2,7 @@
 using AREA_ReST_API;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AREA_ReST_API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231016145933_add_defaultconfig_action_reaction")]
+    partial class add_defaultconfig_action_reaction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AREA_ReST_API/AREA_ReST_API/Migrations/20231016145933_add_defaultconfig_action_reaction.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AREA_ReST_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_defaultconfig_action_reaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DefaultConfiguration",
+                table: "Reactions",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DefaultConfiguration",
+                table: "Actions",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultConfiguration",
+                table: "Reactions");
+
+            migrationBuilder.DropColumn(
+                name: "DefaultConfiguration",
+                table: "Actions");
+        }
+    }
+}
