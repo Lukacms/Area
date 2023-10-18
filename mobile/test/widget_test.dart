6786d6fcd1a6a079227e86532d06987dc5f761c1
Elliot Janvier <elliot.janvier@epitech.eu>
2023-10-18 21:31:31 +0200

    [ADD] added some other widget and unit tests, totalling at 8

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -8,7 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile/back/services.dart';
+import 'package:mobile/screens/login/forgot_password.dart';
 import 'package:mobile/screens/login/login.dart';
+import 'package:mobile/screens/register.dart';
+
+bool isValidEmail(String email) {
+  final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  return emailRegex.hasMatch(email);
+}
 
 void offlineTests() {
   List<dynamic> actionListTest = [
@@ -54,19 +61,45 @@ void offlineTests() {
     expect(AppServices().serviceParse(servicesListTest).first.name,
         'Test service');
   });
+  test('testing the valid email checking function', () {
+    expect(isValidEmail("okokokok"), false);
+  });
+  test('testing the valid email checking function', () {
+    expect(isValidEmail("elliotjanvier@gmail.com"), true);
+  });
 }
 
 void widgetTests() {
   testWidgets(
       'LoginWidget shows username and password fields and a login button',
       (WidgetTester tester) async {
-    // Build the LoginWidget in the test environment.
-    await tester.pumpWidget(MaterialApp(home: Scaffold(body: LoginScreen())));
+    await tester
+        .pumpWidget(const MaterialApp(home: Scaffold(body: LoginScreen())));
 
-    // Verify the presence of the username and password fields and the login button.
     expect(find.byKey(const Key('usernameField')), findsOneWidget);
     expect(find.byKey(const Key('passwordField')), findsOneWidget);
     expect(find.byKey(const Key('loginButton')), findsOneWidget);
+  });
+  testWidgets('Forgot password displays email field and reset password button',
+      (WidgetTester tester) async {
+    await tester
+        .pumpWidget(const MaterialApp(home: Scaffold(body: ForgotPassword())));
+
+    expect(find.byKey(const Key('emailField')), findsOneWidget);
+    expect(find.byKey(const Key('resetPasswordButton')), findsOneWidget);
+  });
+  testWidgets(
+      'Sign up screen displays name, surname, username, email, password fields and signup button',
+      (WidgetTester tester) async {
+    await tester
+        .pumpWidget(const MaterialApp(home: Scaffold(body: RegisterScreen())));
+
+    expect(find.byKey(const Key('nameField')), findsOneWidget);
+    expect(find.byKey(const Key('surnameField')), findsOneWidget);
+    expect(find.byKey(const Key('usernameField')), findsOneWidget);
+    expect(find.byKey(const Key('emailField')), findsOneWidget);
+    expect(find.byKey(const Key('passwordField')), findsOneWidget);
+    expect(find.byKey(const Key('signupButton')), findsOneWidget);
   });
 }
 
