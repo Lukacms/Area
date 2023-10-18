a994ca632b0038a88824416b6545391ff95adda6
Elliot Janvier <elliot.janvier@epitech.eu>
2023-10-18 21:12:24 +0200

    [ADD] added the first three unit tests

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -5,26 +5,55 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile/back/services.dart';
 
-import 'package:mobile/main.dart';
+void offlineTests() {
+  List<dynamic> actionListTest = [
+    {
+      'serviceId': 1,
+      'id': 1,
+      'name': 'Turn on',
+      'endpoint': "Test endpoint",
+      'defaultConfiguration': {
+        'test': 'test',
+      },
+    },
+  ];
+  test('testing the parsing of actions as returned from the server', () {
+    expect(AppServices().actionParse(actionListTest).first.name, 'Turn on');
+  });
+  List<dynamic> reactionListTest = [
+    {
+      'serviceId': 1,
+      'id': 1,
+      'name': 'Turn on',
+      'endpoint': "Test endpoint",
+      'defaultConfiguration': {
+        'test': 'test',
+      },
+    },
+  ];
+  test('testing the parsing of reactions as returned from the server', () {
+    expect(AppServices().actionParse(reactionListTest).first.name, 'Turn on');
+  });
+  List<dynamic> servicesListTest = [
+    {
+      'id': 1,
+      'name': 'Test service',
+      'connectionLink': "Test connection link",
+      'endpoint': "Test endpoint",
+      'svgIcon': "Test svg icon",
+      'iconColor': "Test icon color",
+      'category': "Test category",
+    },
+  ];
+  test('testing the parsing of services as returned from the server', () {
+    expect(AppServices().serviceParse(servicesListTest).first.name,
+        'Test service');
+  });
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  offlineTests();
 }
