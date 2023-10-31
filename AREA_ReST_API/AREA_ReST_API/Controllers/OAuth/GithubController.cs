a9a5de02c009a451390245d7a90e26add879bcd8
Kane <louis.bassagal@epitech.eu>
2023-10-31 11:54:05 +0100

    [ADD] Add endpoint for Oauth2 Mobile

--- AREA_ReST_API/AREA_ReST_API/Controllers/OAuth/GithubController.cs
+++ AREA_ReST_API/AREA_ReST_API/Controllers/OAuth/GithubController.cs
@@ -45,5 +45,27 @@ public class GithubController
         await _context.SaveChangesAsync();
         return new OkResult();
     }
+
+    [HttpPost("mobile")]
+    public async Task<ActionResult> RequestGithubTokenMobile([FromBody] GithubModel githubCode, [FromHeader] string authorization)
+    {
+        var decodedUser = JwtDecoder.Decode(authorization);
+        const string callbackUri = "area%3A%2F%2Foauth2redirect";
+        var query = $"?client_id=Iv1.f47bfd491f94b532&client_secret=c8f7c650f3d4c47462ddbf0ca06b1113478c9f6e&code={githubCode.Code}&redirect_uri={callbackUri}";
+        var result = await _client.PostWithQueryAsync(_githubUri, query, "application/x-www-forms-urlencoded", "application/json");
+        var jsonRes = JObject.Parse(result);
+        var userService = new UserServicesModel
+        {
+            ServiceId = _context.Services.First(service => service.Name == "Github").Id,
+            UserId = decodedUser.Id,
+            AccessToken = jsonRes["access_token"]!.ToString(),
+            RefreshToken = jsonRes["refresh_token"]!.ToString(),
+            ExpiresIn = (int)jsonRes["expires_in"]!,
+        };
+        Console.WriteLine(jsonRes);
+        _context.UserServices.Add(userService);
+        await _context.SaveChangesAsync();
+        return new OkResult();
+    }
 }
 
--- AREA_ReST_API/AREA_ReST_API/Controllers/OAuth/MicrosoftController.cs
+++ AREA_ReST_API/AREA_ReST_API/Controllers/OAuth/MicrosoftController.cs
@@ -56,4 +56,36 @@ public class MicrosoftController
         await _context.SaveChangesAsync();
         return new OkResult();
     }
+
+    [HttpPost("mobile")]
+    public async Task<ActionResult> RequestMicrosoftTokenMobile([FromBody] MicrosoftModel microsoftCode,
+        [FromHeader] string authorization)
+    {
+        var decodedUser = JwtDecoder.Decode(authorization);
+        var callbackUri = "area://oatuh2redirect";
+        var authentication = "";
+        var base64str = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(authentication));
+        var data = new Dictionary<string, string>()
+        {
+            {"client_id", "5731d8cc-7d4b-47dc-812f-f4615f65b38d"},
+            {"client_secret", "eHV8Q~MgohheH_~OxgTyRgbht8RvdEIZ5MkWQc50"},
+            {"grant_type", "authorization_code"},
+            {"code", microsoftCode.Code},
+            {"redirect_uri", callbackUri}
+        };
+        var result = await _client.PostAsync(_microsoftUri, data, "application/x-www-forms-urlencoded", base64str);
+        var microsoftService = _context.Services.First(s => s.Name == "Microsoft");
+        var jsonRes = JObject.Parse(result);
+        var microsoftUserService = new UserServicesModel
+        {
+            UserId = decodedUser.Id,
+            ServiceId = microsoftService.Id,
+            AccessToken = jsonRes["access_token"]!.ToString(),
+            RefreshToken = jsonRes["refresh_token"]!.ToString(),
+            ExpiresIn = (int)jsonRes["expires_in"]!,
+        };
+        _context.UserServices.Add(microsoftUserService);
+        await _context.SaveChangesAsync();
+        return new OkResult();
+    }
 }
